d063bd6988219c45fd6741fe1b5e15bd746b7854
Zahra Keshtkar <zahrakeshtkar@Zahras-MacBook-Pro.local>
2023-01-10 07:02:24 +0330

    feat: check the url is valid or not

--- model/link.go
+++ model/link.go
@@ -3,6 +3,8 @@ package model
 import (
 	"crypto/md5"
 	"encoding/base64"
+	"net/http"
+	"net/url"
 )
 
 type Link struct {
@@ -16,4 +18,12 @@ func NewLink(url string) {
 	md5 := md5.Sum([]byte(url))
 	hash := base64.StdEncoding.EncodeToString(md5[:])
 	l.hash = hash[:6]
+}
+func IsUrlValid(longUrl string) bool {
+	_, err := url.ParseRequestURI(longUrl)
+	return err == nil
+}
+func IsLinkExits(longUrl string) bool {
+	_, err := http.Head(longUrl)
+	return err == nil
 }
