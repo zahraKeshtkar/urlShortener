bb468467f96ddeeb8ebc63fb5b6159d923ba064b
Zahra Keshtkar <zahrakeshtkar@Zahras-MacBook-Pro.local>
2023-01-10 12:17:17 +0330

    Feat: added log for reques and response data

--- main.go
+++ main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/sirupsen/logrus"
 
 	"url-shortner/handler"
 )
 
 func main() {
 	e := echo.New()
+	log := logrus.New()
+
 	e.POST("/new", handler.SaveUrl)
 	e.GET("/:hash", handler.Redirect)
+
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:    true,
+		LogStatus: true,
+		LogValuesFunc: func(c echo.Context, values middleware.RequestLoggerValues) error {
+			log.WithFields(logrus.Fields{
+				"URI":    values.URI,
+				"status": values.Status,
+			}).Info("request")
+
+			return nil
+		},
+	}))
 	e.Start(":8080")
 }
